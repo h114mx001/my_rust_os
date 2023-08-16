f7fc02d4fec63874ed09bab98e32201c74d9f8f7
Tuan Anh Nguyen <21anh.nt@vinuni.edu.vn>
2023-08-16 22:41:01 +0700

    Update test location in main.rs

--- src/main.rs
+++ src/main.rs
@@ -44,15 +44,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap init failed!");
 
-    let mut executor = Executor::new();
-    executor.spawn(Task::new(keyboard::print_keypresses()));
-    executor.run();
-
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
-    rust_os::hlt_loop();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 }
 
 // Call on panic
